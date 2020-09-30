946fd5ce3c7eaa089256a643f5fe7496ae036e20
vladilen <vlad_moryakov_prog@mail.ru>
2020-09-30 07:12:07 +0300

    fsdfs

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const path = require('path')
+const fs = require('fs')
+
+const app = express()
+
+app.use('/static', express.static(path.join(__dirname, 'public')))
+
+app.set('view engine', 'pug')
+
+app.get('/', (req, res) => {
+  res.render('index')
+})
+
+app.get('/curse/:curseId', (req, res) => {
+  if (fs.existsSync(`./views/${req.params.curseId}.pug`)) {
+    res.render(req.params.curseId)
+  } else {
+    res.render('404')
+  }
+})
+
+app.get('/teachers', (req, res) => {
+  res.render('teachers')
+})
+
+app.get('*', (req, res) => {
+  res.render('404')
+})
+
+app.listen(3000, () => {
+  console.log('server is started on port 3000')
+})
